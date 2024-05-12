4c15500224a88eddfc53c36a234293e1c3ed964b
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-05-12 21:33:20 +0530

    almost done

---
+++ app/src/main/java/com/example/taskapp/LogoActivity.kt
@@ -0,0 +1,23 @@
+package com.example.taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.content.Intent
+
+class LogoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Handler to delay navigation
+        Handler().postDelayed({
+            // Intent to navigate to OnboardActivity01
+  //          val intent = Intent(this, OnboardActivity01::class.java)
+            startActivity(intent)
+            // Finish current activity if you don't want to go back to it
+            finish()
+        }, 3000) // 3000 milliseconds = 3 seconds
+    }
+}
---
+++ app/src/main/java/com/example/taskapp/OnboardActivity03.kt
@@ -0,0 +1,25 @@
+package com.example.taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class OnboardActivity03 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard03)
+
+        // Get Started button click listener
+        val getStartedButton: Button = findViewById(R.id.button5)
+        getStartedButton.setOnClickListener {
+            navigateToLoginActivity()
+        }
+    }
+
+    private fun navigateToLoginActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+        finish() // Finish this activity to prevent going back to it
+    }
+}
